81fa331524fd180996f6718365d741b60df759f3
Bob <zhang.zebedy@gmail.com>
2022-03-09 16:17:37 +0800

    合并shell脚本

--- second.sh
+++ second.sh
@@ -1,33 +1,98 @@
 #!/bin/bash
 # https://github.com/deplives/OpenWrt-CI
 
-# Run after feeds install
+# Run before feeds install
 
-# Delete dns query in page connections
-echo 'Delete dns query in page connections...'
-if [ $(grep -c 'admin/status/nameinfo' package/feeds/luci/luci-mod-admin-full/luasrc/view/admin_status/connections.htm) -ne '0' ]; then
-    start_line=$(grep -n 'if (lookup_queue.length > 0)' package/feeds/luci/luci-mod-admin-full/luasrc/view/admin_status/connections.htm | awk -F : '{printf $1}')
-    end_line=$(grep -n 'var data = json.statistics;' package/feeds/luci/luci-mod-admin-full/luasrc/view/admin_status/connections.htm | awk -F : '{printf $1 - 1}')
-    sed -i "${start_line},${end_line}d" package/feeds/luci/luci-mod-admin-full/luasrc/view/admin_status/connections.htm
+COMMIT_SHA=$1
+if [ -z "$COMMIT_SHA" ]; then
+    COMMIT_SHA='Unknown'
 fi
 
-# Delete placeholder in dhcp.lua
-echo 'Delete bogusnxdomain placeholder...'
-sed -i '/67.215.65.132/d' feeds/luci/modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/dhcp.lua
+# Modify default timezone
+echo 'Modify default timezone...'
+sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
 
-echo 'Delete dns server placeholder...'
-sed -i '/10.1.2.3/d' feeds/luci/modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/dhcp.lua
+# Modify default NTP server
+echo 'Modify default NTP server...'
+sed -i 's/ntp.aliyun.com/ntp.ntsc.ac.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/time1.cloud.tencent.com/ntp.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/time.ustc.edu.cn/cn.ntp.org.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/cn.pool.ntp.org/pool.ntp.org/g' package/base-files/files/bin/config_generate
 
-echo 'Delete rebind domain placeholder...'
-sed -i '/ihost.netflix.com/d' feeds/luci/modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/dhcp.lua
+# Modify default LAN ip
+echo 'Modify default LAN IP...'
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
-echo 'Add i18n in base.po...'
-cat <<EOF >>feeds/luci/modules/luci-base/po/zh-cn/base.po
+# Modify default theme
+echo 'Modify default theme...'
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-msgid "Build Version"
-msgstr "编译版本"
+# Modify default luci-app-turboacc
+# https://github.com/deplives/luci-app-turboacc
+echo 'Modify default luci-app-turboacc...'
+rm -rf package/lean/luci-app-turboacc && git clone https://github.com/deplives/luci-app-turboacc.git package/lean/luci-app-turboacc
 
-msgid "Build Date"
-msgstr "编译日期"
+# Modify default ttyd and luci-app-ttyd
+# https://github.com/deplives/ttyd
+# https://github.com/deplives/luci-app-ttyd
+echo 'Modify default ttyd and luci-app-ttyd...'
+rm -rf feeds/packages/utils/ttyd && git clone https://github.com/deplives/ttyd.git feeds/packages/utils/ttyd
+rm -rf package/lean/luci-app-ttyd && git clone https://github.com/deplives/luci-app-ttyd.git package/lean/luci-app-ttyd
 
+# Modify zzz-default-settings
+echo 'Delete `sed -i 's#downloads.openwrt.org#mirrors.cloud.tencent.com/lede#g' /etc/opkg/distfeeds.conf`...'
+sed -i '/mirrors.cloud.tencent.com/d' package/lean/default-settings/files/zzz-default-settings
+
+# Modify admin/status/overview <td id="wan4_i" style="width:16px; text-align:center; padding:3px">
+echo 'Modify admin/status/overview <td id="wan4_i" style="width:16px; text-align:center; padding:3px">...'
+sed -i 's/<td id="wan4_i" style="width:16px; text-align:center; padding:3px">/<td id="wan4_i" style="width:10%; text-align:center; padding:3px">/g' package/lean/autocore/files/x86/index.htm
+
+# Modify admin/status/overview <td id="wan6_i" style="width:16px; text-align:center; padding:3px">
+echo 'Modify admin/status/overview <td id="wan6_i" style="width:16px; text-align:center; padding:3px">...'
+sed -i 's/<td id="wan6_i" style="width:16px; text-align:center; padding:3px">/<td id="wan6_i" style="width:10%; text-align:center; padding:3px">/g' package/lean/autocore/files/x86/index.htm
+
+# Modify localtime in Homepage
+echo 'Modify localtime in Homepage...'
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/x86/index.htm
+
+# Add Build Version in Homepage
+echo 'Add Build Version in Homepage...'
+line_kernel_version=$(grep -n 'Kernel Version' package/lean/autocore/files/x86/index.htm | awk -F ':' '{print $1}')
+sed -i "${line_kernel_version}a\                <tr><td width=\"33%\"><%:Build Version%></td><td>Stable</td></tr>" package/lean/autocore/files/x86/index.htm
+
+# Add Build Date in Homepage
+echo 'Add Build Date in Homepage...'
+build_date=$(date +"%Y-%m-%d")
+line_build_version=$(grep -n 'Build Version' package/lean/autocore/files/x86/index.htm | awk -F ':' '{print $1}')
+sed -i "${line_build_version}a\                <tr><td width=\"33%\"><%:Build Date%></td><td>${build_date}</td></tr>" package/lean/autocore/files/x86/index.htm
+
+# Modify hostname in Homepage
+echo 'Modify hostname...'
+sed -i 's/${g}'"'"' - '"'"'//g' package/lean/autocore/files/x86/autocore
+
+# Replace openwrt.org in diagnostics with www.baidu.com
+echo 'Replace openwrt.org in diagnostics.htm with www.baidu.com...'
+sed -i "/exit 0/d" package/lean/default-settings/files/zzz-default-settings
+cat <<EOF >>package/lean/default-settings/files/zzz-default-settings
+uci set luci.diag.ping=www.baidu.com
+uci set luci.diag.route=www.baidu.com
+uci set luci.diag.dns=www.baidu.com
+uci commit luci
+
+exit 0
 EOF
+
+# Modify default banner
+echo 'Modify default banner...'
+echo "                                                               " >package/base-files/files/etc/banner
+echo " ██████╗ ██████╗ ███████╗███╗   ██╗██╗    ██╗██████╗ ████████╗ " >>package/base-files/files/etc/banner
+echo "██╔═══██╗██╔══██╗██╔════╝████╗  ██║██║    ██║██╔══██╗╚══██╔══╝ " >>package/base-files/files/etc/banner
+echo "██║   ██║██████╔╝█████╗  ██╔██╗ ██║██║ █╗ ██║██████╔╝   ██║    " >>package/base-files/files/etc/banner
+echo "██║   ██║██╔═══╝ ██╔══╝  ██║╚██╗██║██║███╗██║██╔══██╗   ██║    " >>package/base-files/files/etc/banner
+echo "╚██████╔╝██║     ███████╗██║ ╚████║╚███╔███╔╝██║  ██║   ██║    " >>package/base-files/files/etc/banner
+echo " ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═══╝ ╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝    " >>package/base-files/files/etc/banner
+echo " ------------------------------------------------------------- " >>package/base-files/files/etc/banner
+echo " %D %C ${build_date} Stable @Bobby                             " >>package/base-files/files/etc/banner
+echo " $COMMIT_SHA                                                   " >>package/base-files/files/etc/banner
+echo " ------------------------------------------------------------- " >>package/base-files/files/etc/banner
+echo "                                                               " >>package/base-files/files/etc/banner
